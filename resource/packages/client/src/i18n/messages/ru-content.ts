2c04b59aef7ec97d04cd77111ebcadf828973233
oofsdfs <oofsdfs@yandex.com>
2026-09-22 18:03:24 +0300

    Update ru-content.ts

--- resource/packages/client/src/i18n/messages/ru-content.ts
+++ resource/packages/client/src/i18n/messages/ru-content.ts
@@ -1,5 +1,5 @@
-import drafts from "./en-content-drafts.js";
-import accessGroups from "./en-content-accessGroups.js";
-import error from "./en-content-error.js";
-import common from "./en-content-common.js";
+import drafts from "./ru-content-drafts.js";
+import accessGroups from "./ru-content-accessGroups.js";
+import error from "./ru-content-error.js";
+import common from "./ru-content-common.js";
 export default { drafts, accessGroups, error, common } as const;
